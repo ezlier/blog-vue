b77c2318d6173de3a42b7471850416a4b92dc87c
ezlier <86654833@qq.com>
2025-08-05 11:07:11 +0800

    text

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import file from '@/views/file/index.vue'
@@ -8,7 +8,7 @@ import PostDetail from '@/views/PostDetail.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   
     routes:[
         {
